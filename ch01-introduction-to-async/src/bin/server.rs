89fb2812dab49219bda3ab3fdc6f75974107a612
ohmycloud <ohmycloudy@gmail.com>
2024-10-28 23:51:24 +0800

    use tokio to reduce the number of moving parts

--- ch01-introduction-to-async/src/bin/server.rs
+++ ch01-introduction-to-async/src/bin/server.rs
@@ -1,14 +1,37 @@
-use std::process::{Command, Output};
+use std::process::Command;
 
-fn main() {
-    let output: Output = Command::new("./connection")
-        .output()
-        .expect("Failed to execute command");
-    if output.status.success() {
-        let stdout = String::from_utf8_lossy(&output.stdout);
-        println!("Output: {}", stdout);
-    } else {
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        eprintln!("Error: {}", stderr);
+#[tokio::main]
+async fn main() {
+    let mut threads = vec![];
+    for _ in 0..4 {
+        let handle = tokio::spawn(async {
+            let output = Command::new("./connection").output();
+            match output {
+                Ok(output) => {
+                    println!(
+                        "Process completed with output: {}",
+                        String::from_utf8_lossy(&output.stdout)
+                    );
+                    Ok(output.status.code().unwrap_or(-1))
+                }
+                Err(e) => {
+                    eprint!("Failed to run process: {}", e);
+                    Err(e)
+                }
+            }
+        });
+        threads.push(handle);
+    }
+
+    let mut results = Vec::with_capacity(threads.len());
+    for handle in threads {
+        results.push(handle.await.unwrap());
+    }
+
+    for (i, result) in results.into_iter().enumerate() {
+        match result {
+            Ok(exit_code) => println!("Process {} exited with code {}", i + 1, exit_code),
+            Err(e) => eprint!("Process {} failed: {}", i + 1, e),
+        }
     }
 }
